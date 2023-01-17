06a5ad0f18736c1468d8970350c1e176e4b00b10
danielrizydy <danielrizkydy@gmail.com>
2023-01-17 10:27:26 +0700

    Upload Pertama

---
+++ app/views/partials/appfooter.php
@@ -0,0 +1,12 @@
+<footer class="footer border-top">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col">
+				<div class="footer-links text-left">
+					
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</footer>
